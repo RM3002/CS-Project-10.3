aeb47c50ca014808a04d57a6d84d3822d0f073af
Rayan Mouk <rayansemail@duck.com>
2026-03-09 15:55:49 +0100

    added odd feautre02

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 from feature01 import return_even
-
+from feature02 import return_odd 
 original_list = [i for i in range(10)]
 
 even_list = return_even(original_list)
 
+odd_list= return_odd
 
 st.write("hooray we connected everything")
 
