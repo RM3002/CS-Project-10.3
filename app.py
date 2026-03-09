687a25393e2b3515e76feba8407b713ca1ffb88a
Rayan Mouk <rayansemail@duck.com>
2026-03-09 16:04:38 +0100

    c

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-import streamlit as st
-
-from feature01 import return_even
-from feature02 import return_odd 
-original_list = [i for i in range(10)]
-
-even_list = return_even(original_list)
-
-odd_list= return_odd(original_list)
-
-st.write("hooray we connected everything")
-
-st.write(even_list)
-
-st.write(odd_list)
 
